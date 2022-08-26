85b13283fec9e498fed53b3f0a870b198fb865b1
Miguel Hugo <dragonmike6@gmail.com>
2022-08-26 18:53:38 -0500

    we modify the tag of the feature

--- petStore karate/petstore/src/test/java/karate/petstore/DeleteAPet.java
+++ petStore karate/petstore/src/test/java/karate/petstore/DeleteAPet.java
@@ -4,6 +4,6 @@ import com.intuit.karate.junit5.Karate;
 public class DeleteAPet {
     @Karate.Test
     Karate sampleTest(){
-        return Karate.run("classpath:karate/PetStore/DeletePet/DeletesAPet.feature").relativeTo(getClass());
+        return Karate.run("classpath:karate/PetStore/ZDeletePet/DeletesAPet.feature").relativeTo(getClass());
     }
 }
