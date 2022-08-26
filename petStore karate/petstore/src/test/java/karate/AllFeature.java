1b4f6c384270da14a0c377cbb512b933091c9e7c
Miguel Hugo <dragonmike6@gmail.com>
2022-08-26 18:52:42 -0500

    we modify the order of the tags

--- petStore karate/petstore/src/test/java/karate/AllFeature.java
+++ petStore karate/petstore/src/test/java/karate/AllFeature.java
@@ -13,7 +13,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-@KarateOptions(tags = {"@addNewPet,@findPetById,@updatePet,~@delete"})
+@KarateOptions(tags = {"@addNewPet,@findPetById,@updatePet,@Zdelete"})
 
 public class AllFeature {
     public static void generateReport(String karateOutputPath){
